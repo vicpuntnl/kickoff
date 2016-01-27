6ddced0ac4fa7294a61e4c527b99ca3680380032
Richard van der Pol <richardvanderpol@gmail.com>
2016-01-27 12:56:52 +0100

    Path fix

--- frontend/build/config/paths.js
+++ frontend/build/config/paths.js
@@ -5,9 +5,9 @@
 /**
  * Root paths
  */
-global.setRoot('dest',              'frontend/dist');
-global.setRoot('src',               'frontend/src');
-global.setRoot('styleguide',        'frontend/styleguide');
+global.setRoot('dest',              './frontend/dist');
+global.setRoot('src',               './frontend/src');
+global.setRoot('styleguide',        './frontend/styleguide');
 
 /**
  * Destination paths
