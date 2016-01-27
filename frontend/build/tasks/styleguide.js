67f8e94e992697039c19a3da8a9241f00090b7bb
Richard van der Pol <richardvanderpol@gmail.com>
2016-01-27 12:55:50 +0100

    Renewed styleguide watch task

--- frontend/build/tasks/styleguide.js
+++ frontend/build/tasks/styleguide.js
@@ -1,45 +1,68 @@
 /**
- * Copy project files to Patternlab
+ * Copy project files to Styleguide folders
  */
 var gulp = require('gulp'),
-	rename = require('gulp-rename'),
-	dest = global.getRoot('dest'),
-    styleguidePath = global.getRoot('styleguide'), // patternlab root path
+    watch = require('gulp-watch'),
+    watchAll = '/**/*',
     styleguidePathPublic = global.getRoot('styleguide') + '/public', // path to public folder of patternlab
     styleguidePathSrc = global.getRoot('styleguide') + '/source'; // path to source folder of patternlab
 
 
-function copyJs(){
-    return gulp.src(dest + '/js/*.js')
+/**
+ * Copy functions 
+ */
+function copyJs() {
+    return gulp.src(global.getDest('js') + watchAll)
         .pipe(gulp.dest(styleguidePathSrc + '/js'));
 }
 
-// Css goes directly to public folder of patternlab (patternlab does not do this out of the box for css)
-function copyCss(){
-    return gulp.src(dest + '/css/style.css')
+function copyCss() {
+    return gulp.src(global.getDest('scss') + watchAll)
         .pipe(gulp.dest(styleguidePathPublic + '/css'));
 }
 
-function copyImg(){
-    return gulp.src(dest + '/img/*.*')
+function copyFonts(){
+    return gulp.src(global.getDest('font') + watchAll)
+        .pipe(gulp.dest(styleguidePathSrc + '/fonts'));
+}
+
+function copyImg() {
+    return gulp.src(global.getDest('img') + watchAll)
         .pipe(gulp.dest(styleguidePathSrc + '/images'));
 }
 
-function copyFonts(){
-    return gulp.src(dest + '/font/*.*')
-        .pipe(gulp.dest(styleguidePathSrc + '/fonts'));
+
+
+/**
+ * Watch all generated files in dist folder 
+ */
+function watchDistFolder(folder, func) {
+    watch(
+        global.getDest(folder) + watchAll,
+        func
+    );
+}
+
+function watchDistFiles() {
+    watchDistFolder('js', copyJs);
+    watchDistFolder('scss', copyCss);
+    watchDistFolder('font', copyFonts);
+    watchDistFolder('img', copyImg);
 }
 
 
 /**
- * Copies all generated files from the dist to the patternlab source folder
+ * Copy all generated files in dist folder to styleguide
  */
- function copyFiles() {
- 	copyJs();
- 	copyCss();
- 	copyImg();
- 	copyFonts();
- }
+function copyDistFiles() {
+    copyJs();
+    copyCss();
+    copyFonts();
+    copyImg();
+}
 
 
- gulp.task('styleguide-copy', copyFiles);
+
+gulp.task('styleguide:watch', watchDistFiles); // Set watcher on dist folder. Copy files on change to styleguide
+
+gulp.task('styleguide:copy', copyDistFiles); // Copy all dist files once to styleguide 
