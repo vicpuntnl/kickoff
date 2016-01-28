2c0f2e59133fe1e73c7040efc5e518c56249e2dd
Richard van der Pol <richardvanderpol@gmail.com>
2016-01-28 10:27:32 +0100

    Rebuild of iconfonts task
    Now has source in '/scr/icons' and generates iconfont in '/dist/icons'

--- frontend/build/tasks/iconfont.js
+++ frontend/build/tasks/iconfont.js
@@ -2,39 +2,39 @@ var gulp = require('gulp'),
     iconfont = require('gulp-iconfont'),
     iconfontCss = require('gulp-iconfont-css'),
     runTimestamp = Math.round(Date.now() / 1000),
-    src = global.getSrc('icon'),
-    fontSrc = './Frontend/src/font/',
-    dest = global.getDest('font'),
-    target = global.getSrc('css'),
-    configFile = 'Frontend/build/config/iconfont.scss';
+    iconfontName = 'icon-font',
+    src = global.getSrc('icons'), // source files (svg's & config)
+    svgSrc = src + '/*.svg', // svg icons location
+    template = src + '/_iconfont.template.scss', // config file for generating '_generated.icons.scss'
+    dest = global.getDest('icons'), // destination file to place fonts
+    generatedScssInclude = '../../../src/icons/_generated.icons.scss'; // include sccs file with generated icon classes (relative path!!)
 
 /**
  * Copies font from the source to the destination folder
  */
-    function createIconFont() {
-        var runTimestamp = Math.round(Date.now() / 1000);
+ function createIconFont() {
+     var runTimestamp = Math.round(Date.now() / 1000);
 
-        console.log(runTimestamp);
      return gulp
-         .src(src)
+         .src(svgSrc)
             .pipe(
                 iconfontCss({
-                    fontName: 'icon-font',
-                    path: configFile, 
-                    targetPath: '../../src/scss/02-tools/_icons.scss',
-                    fontPath: dest
+                    path: template, // The template path
+                    fontName: iconfontName, // The name of the generated font family (required)
+                    fontPath: dest, // Directory of font files relative to generated (S)CSS file (optional, defaults to ./)
+                    targetPath: '../../../src/icons/_generated.icons.scss' // The path where the (S)CSS file should be saved, relative to the path used in gulp.dest()
                 })
             ) 
             .pipe(
                 iconfont({
-                    fontName: 'icon-font',
+                    fontName: iconfontName,
                     formats: ['woff', 'ttf'],
                     timestamp: runTimestamp,
                     normalize: true
                 })
             )
 
-        .pipe(gulp.dest(fontSrc));
-    }
+        .pipe(gulp.dest(dest));
+}
 
-    gulp.task('iconfont', createIconFont);
+gulp.task('iconfont', createIconFont);
